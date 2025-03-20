dd8b21f27d3fb1ce207ed3d8925f45570c77b7ba
Noah Giboney <noahgiboney@gmail.com>
2025-03-20 15:22:14 -0700

    updated site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import { Kanit } from "next/font/google";
 
 const kanit = Kanit({
